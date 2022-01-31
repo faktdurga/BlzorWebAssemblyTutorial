e097b751f2ccf466b91ddc443323d6f0eba2a2bf
faktdurga <mohited24@gmail.com>
2022-01-31 21:54:36 +0530

    Add project files.

---
+++ BlazorProject/Server/Controllers/EmployeesController.cs
@@ -0,0 +1,163 @@
+﻿using BlazorProject.Server.Model;
+using BlazorProject.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProject.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository employeeRepository;
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetEmployees()
+        {
+            try
+            {
+                return Ok(await employeeRepository.GetEmployees());
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error for retrieving data from database");
+            }
+            
+        }
+
+
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Employee>> GetEmployees(int id)
+        {
+            try
+            {
+                var result = await employeeRepository.GetEmployee(id);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return result;
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error for retrieving data from database");
+            }
+            
+        }
+
+
+       
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            try
+            {
+                if (employee == null)
+                    return BadRequest();
+
+                var emp = await employeeRepository.GetEmployeeByEmail(employee.Email);
+
+                if (emp != null)
+                {
+                    ModelState.AddModelError("Email", "Employee email already in use");
+                    return BadRequest(ModelState);
+                }
+
+                var createdEmployee = await employeeRepository.AddEmployee(employee);
+
+                return CreatedAtAction(nameof(GetEmployees),
+                    new { id = createdEmployee.EmployeeId }, createdEmployee);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error creating new employee record");
+            }
+        }
+
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<Employee>> UpdateEmployee(int id, Employee employee)
+        {
+            try
+            {
+                if (id != employee.EmployeeId)
+                    return BadRequest("Employee ID mismatch");
+
+                var employeeToUpdate = await employeeRepository.GetEmployee(id);
+
+                if (employeeToUpdate == null)
+                {
+                    return NotFound($"Employee with Id = {id} not found");
+                }
+
+                return await employeeRepository.UpdateEmployee(employee);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error updating employee record");
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteEmployee(int id)
+        {
+            try
+            {
+                var employeeToDelete = await employeeRepository.GetEmployee(id);
+
+                if (employeeToDelete == null)
+                {
+                    return NotFound($"Employee with Id = {id} not found");
+                }
+
+                await employeeRepository.DeleteEmployee(id);
+
+                return Ok($"Employee with Id = {id} deleted");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error deleting employee record");
+            }
+        }
+
+        //public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        //{
+        //    try
+        //    {
+        //        if (employee == null)
+        //            return BadRequest();
+
+        //        var createdEmployee = await employeeRepository.AddEmployee(employee);
+
+
+        //        return CreatedAtAction(nameof(GetEmployees),
+        //              new { id = createdEmployee.EmployeeId }, createdEmployee);
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        return StatusCode(StatusCodes.Status500InternalServerError, "Error for creating new record");
+        //    }
+
+        //}
+
+
+
+    }
+}
---
+++ BlazorProject/Server/Model/IEmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorProject.Shared;
+
+namespace BlazorProject.Server.Model
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> Search(string name, Gender? gender);
+        Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee> GetEmployee(int employeeID);
+        Task<Employee> GetEmployeeByEmail(string email);
+        Task<Employee> AddEmployee(Employee employee);
+
+        Task<Employee> UpdateEmployee(Employee employee);
+        Task DeleteEmployee(int employeeID);
+        Task GetEmployee(string firstName);
+    }
+}
